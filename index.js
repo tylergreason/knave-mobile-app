6d96c00640432db71cf234f21e0be6671c21a4bb
tylergreason <tyler.greason@protonmail.com>
2020-06-12 16:50:41 -0400

    Added sliding left to close the menu if it is open (active)

--- index.js
+++ index.js
@@ -62,17 +62,34 @@ const slideMenuOut = () => {
 }
 // add 'active' class to drawer to open and close it
 const toggleMenuOpen = () => {
-    // sideDrawer.classList.toggle('non-active'); 
+    /*
+        the code below this was supposed to be for efficiently removing the event listener from the close button if the drawer was closed, as it wouldn't need to be listening for a close action. I couldn't quite get it to work, so I'll come back to it later. 
+    */
+    // // if drawer doesn't have 'active' class, add active class and add event listener for close button 
+    // if (!sideDrawer.classList.contains('active')){
+    //     closeButton.addEventListener('click', () => {
+    //         toggleMenuOpen(); 
+    //     })
+    //     console.log('does not contain active');
+    //     // else remove active class and remove listener from close button 
+    // }else{
+    //     removeEventListenerFromCloseButton()
+    // }
     sideDrawer.classList.toggle('active'); 
 }
+
+closeButton.addEventListener('click', toggleMenuOpen)
+
+// define function to remove event listener from closeButton 
+const removeEventListenerFromCloseButton = () => {
+    closeButton.removeEventListener('click', toggleMenuOpen)
+}
+
 
 toggler.addEventListener('click', () => {
     toggleMenuOpen()
 })
 
-closeButton.addEventListener('click', () => {
-    toggleMenuOpen(); 
-})
 
 let x1, y1; 
 document.addEventListener('touchstart', (event) => {
@@ -89,7 +106,14 @@ document.addEventListener('touchend', event =>{
     let y2 = event.changedTouches[0].clientY; 
     // console.log (x1 - x2); 
     if (x2 - x1 > 20 && x1 < 20){
-        sideDrawer.classList.toggle('active')
+        if (!sideDrawer.classList.contains('active')){
+            sideDrawer.classList.add('active')
+        }
+    }
+    if (x2 - x1 < 20 && x1 < 200){
+        if (sideDrawer.classList.contains('active')){
+            sideDrawer.classList.remove('active')
+        }
     }
 })
 
