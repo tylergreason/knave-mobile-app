ad3faaf5dcb9e985ddf36178634b5aaec19cfb68
tylergreason <tyler.greason@protonmail.com>
2020-06-12 16:04:51 -0400

    Got the drawer to slide open with a sliding touch type action. Working to get the animation smooth.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// find test div 
-let test = document.getElementById('test'); 
-console.log(test); 
+// find sideDrawer div 
+let sideDrawer = document.getElementById('sideDrawer'); 
+let toggler = document.getElementById('toggler')
+let closeButton = document.getElementById('closeMenuButton')
 
 // create keyframes inside an array 
-
 let slideInFrames = [
     {
         background: 'black',
@@ -48,8 +48,8 @@ let timeIn = {
 
 
 const slideMenuIn = () => {
-    // apply animation to test div 
-    return test.animate(slideInFrames, {
+    // apply animation to sideDrawer div 
+    return sideDrawer.animate(slideInFrames, {
         duration: 500,
         easing: 'ease-out',
         iterations: 1
@@ -57,8 +57,40 @@ const slideMenuIn = () => {
 }
 
 const slideMenuOut = () => {
-    // apply animation to test div 
-    return test.animate(slideOutFrames, {duration:200})    
+    // apply animation to sideDrawer div 
+    return sideDrawer.animate(slideOutFrames, {duration:200})    
+}
+// add 'active' class to drawer to open and close it
+const toggleMenuOpen = () => {
+    sideDrawer.classList.toggle('non-active'); 
+    sideDrawer.classList.toggle('active'); 
 }
 
-slideMenuIn() 
+toggler.addEventListener('click', () => {
+    toggleMenuOpen()
+})
+
+closeButton.addEventListener('click', () => {
+    toggleMenuOpen(); 
+})
+
+let x1, y1; 
+document.addEventListener('touchstart', (event) => {
+    console.log('touch moved');
+    let touchLocation = event.targetTouches[0]
+    console.log(touchLocation.pageX);
+    console.log(touchLocation.pageY);
+    x1 = touchLocation.clientX;
+    y1 = touchLocation.clientY;
+})
+
+document.addEventListener('touchend', event =>{
+    let x2 = event.changedTouches[0].clientX; 
+    let y2 = event.changedTouches[0].clientY; 
+    // console.log (x1 - x2); 
+    if (x2 - x1 > 20 && x1 < 20){
+        sideDrawer.classList.toggle('active')
+    }
+})
+
+
