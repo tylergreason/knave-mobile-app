8a05cede73c16baebb108e2ebc916cd1cb46a013
tylergreason <tyler.greason@protonmail.com>
2020-06-15 11:11:08 -0400

    Corrected issue where vertically scrolling in menu drawer would close the menu because of swiping left during vertical scroll

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const drawerWidth = 200; 
 
-const swipeAreaLeft = 0, minimumSwipeDistance = 20, swipeAreaRight = swipeAreaLeft + minimumSwipeDistance; 
+const swipeAreaLeft = 0, minimumSwipeDistance = 60, swipeAreaRight = swipeAreaLeft + minimumSwipeDistance; 
 // set css drawer width to that which is set above 
 document.documentElement.style.setProperty(`--drawerWidth`, `${drawerWidth}px`);
 
@@ -67,21 +67,9 @@ const slideMenuOut = () => {
     // apply animation to sideDrawer div 
     return sideDrawer.animate(slideOutFrames, {duration:200})    
 }
+
 // add 'active' class to drawer to open and close it
 const toggleMenuOpen = () => {
-    /*
-        the code below this was supposed to be for efficiently removing the event listener from the close button if the drawer was closed, as it wouldn't need to be listening for a close action. I couldn't quite get it to work, so I'll come back to it later. 
-    */
-    // // if drawer doesn't have 'active' class, add active class and add event listener for close button 
-    // if (!sideDrawer.classList.contains('active')){
-    //     closeButton.addEventListener('click', () => {
-    //         toggleMenuOpen(); 
-    //     })
-    //     console.log('does not contain active');
-    //     // else remove active class and remove listener from close button 
-    // }else{
-    //     removeEventListenerFromCloseButton()
-    // }
     sideDrawer.classList.toggle('active'); 
 }
 
@@ -93,34 +81,42 @@ const removeEventListenerFromCloseButton = () => {
 }
 
 
-toggler.addEventListener('click', () => {
-    toggleMenuOpen()
-})
+toggler.addEventListener('click', toggleMenuOpen)
 
 
 let x1, y1; 
 document.addEventListener('touchstart', (event) => {
-    console.log('touch moved');
+    // debugger
+    // console.log('touch moved');
     let touchLocation = event.targetTouches[0]
     // console.log(touchLocation.pageX);
     // console.log(touchLocation.pageY);
+    console.log(touchLocation.clientX - event.changedTouches[0].clientX);
+    
+
     x1 = touchLocation.clientX;
     y1 = touchLocation.clientY;
+    // debugger
+
 })
 
-document.addEventListener('touchend', event =>{
+document.addEventListener('touchend', event => {
     let x2 = event.changedTouches[0].clientX; 
     let y2 = event.changedTouches[0].clientY; 
-    // console.log (x1 - x2); 
+    let xDiff = Math.abs(x1 - x2); 
+    let yDiff = Math.abs(y1 - y2); 
+
     // if the minimum distance swiped is (whatever the const set at the top of this page says) and the swipe started inside the swipe area (again, see the top of the page for those consts)
     console.log(`${x1} ${x2}`)
     // debugger
+    // if swiping right 
     if (x2 - x1 > minimumSwipeDistance && x1 < swipeAreaRight){
         if (!sideDrawer.classList.contains('active')){
             sideDrawer.classList.add('active')
         }
     }
-    else if ((Math.abs(x1) - Math.abs(x2) > (minimumSwipeDistance)) && x1 > x2 && x1 < drawerWidth){
+    // if swiping left 
+    else if ((x1 - x2 > minimumSwipeDistance) && x1 < drawerWidth && xDiff > yDiff){
         console.log('swiped back')
         if (sideDrawer.classList.contains('active')){
             sideDrawer.classList.remove('active')
