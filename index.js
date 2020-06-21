01c6245f1cc9bd471d9d6694c9aeaea5a39d4ff4
tylergreason <tyler.greason@protonmail.com>
2020-06-21 17:25:55 -0500

    side drawer is moving with page as intended now in Firefox.

--- index.js
+++ index.js
@@ -1,122 +1,124 @@
 const drawerWidth = 200; 
 
-const swipeAreaLeft = 0, minimumSwipeDistance = 60, swipeAreaRight = swipeAreaLeft + minimumSwipeDistance; 
-// set css drawer width to that which is set above 
-document.documentElement.style.setProperty(`--drawerWidth`, `${drawerWidth}px`);
+// const swipeAreaLeft = 0, minimumSwipeDistance = 60, swipeAreaRight = swipeAreaLeft + minimumSwipeDistance; 
+// // set css drawer width to that which is set above 
+// document.documentElement.style.setProperty(`--drawerWidth`, `${drawerWidth}px`);
 
-// add 'active' class to drawer to open and close it
-const toggleMenuOpen = () => {
-    sideDrawer.classList.toggle('active'); 
-}
+// // add 'active' class to drawer to open and close it
+// const toggleMenuOpen = () => {
+//     sideDrawer.classList.toggle('active'); 
+// }
 
 const closeSideDrawer = () => {
-    if (sideDrawer.classList.contains('active')){
-        sideDrawer.classList.remove('active'); 
-    }
+    sideDrawer.classList.remove('active')
 }
 
-closeMenuButton.addEventListener('click', toggleMenuOpen)
-// define function to remove event listener from closeMenuButton 
-const removeEventListenerFromcloseMenuButton = () => {
-    closeMenuButton.removeEventListener('click', toggleMenuOpen)
+const openSideDrawer = () => {
+    sideDrawer.classList.add('active')
 }
 
-toggler.addEventListener('click', toggleMenuOpen)
+// closeMenuButton.addEventListener('click', toggleMenuOpen)
+// // define function to remove event listener from closeMenuButton 
+// const removeEventListenerFromcloseMenuButton = () => {
+//     closeMenuButton.removeEventListener('click', toggleMenuOpen)
+// }
 
-// swiping menu open touch feature
-let x1, y1; 
-let recentTouches = []; 
-let firstX = ''; 
-document.addEventListener('touchstart', (event) => {
-    if (firstX === ''){
-        firstX = event.changedTouches[0].clientX; 
-        console.log(firstX);
-    }
-    // console.log(event.timeStamp)
-    // check for doubletap, make sure  recentTouches list isn't empty first 
+// toggler.addEventListener('click', toggleMenuOpen)
 
-    if (recentTouches.length > 0){
-        // console.log(recentTouches)
-        // console.log(`${event.timeStamp} - ${recentTouches[recentTouches.length-1]}`);
-        if ((event.timeStamp - recentTouches[recentTouches.length-1]) < 1000){
-            console.log('double tap')
-            recentTouches = [];
-        }
-    }
-    recentTouches = [];
-    recentTouches.push(event.timeStamp); 
-    console.log(recentTouches);
-    let touchLocation = event.targetTouches[0]
-    x1 = touchLocation.clientX;
-    y1 = touchLocation.clientY;
-    // debugger
-})
+// // swiping menu open touch feature
+// let x1, y1; 
+// let recentTouches = []; 
+// let firstX = ''; 
+// document.addEventListener('touchstart', (event) => {
+//     if (firstX === ''){
+//         firstX = event.changedTouches[0].clientX; 
+//         console.log(firstX);
+//     }
+//     // console.log(event.timeStamp)
+//     // check for doubletap, make sure  recentTouches list isn't empty first 
 
-
-document.addEventListener('touchmove', (event)=>{
-    // console.log(event.touches[0].clientY)
-    // console.log(event.touches[0].clientX)
-
-    // toggler.style.left = event.touches[0].pageX + 'px'
-    // toggler.style.top = event.touches[0].pageY + 'px'
-    if (event.touches[0].clientX > firstX + 20){
-        // move drawer out 
-        sideDrawer.style.left = event.touches[0].clientX;
-    }
-
-})
-
-document.addEventListener('touchend', event => {
-    let x2 = event.changedTouches[0].clientX; 
-    let y2 = event.changedTouches[0].clientY; 
-    let xDiff = Math.abs(x1 - x2); 
-    let yDiff = Math.abs(y1 - y2); 
-
-    // if the minimum distance swiped is (whatever the const set at the top of this page says) and the swipe started inside the swipe area (again, see the top of the page for those consts)
-    // console.log(`${x1} ${x2}`)
-    // debugger
-    // if swiping right 
-    if (x2 - x1 > minimumSwipeDistance && x1 < swipeAreaRight){
-        if (!sideDrawer.classList.contains('active')){
-            sideDrawer.classList.add('active')
-        }
-    }
-    // if swiping left 
-    else if ((x1 - x2 > minimumSwipeDistance) && x1 < drawerWidth && xDiff > yDiff){
-        // console.log('swiped back')
-        if (sideDrawer.classList.contains('active')){
-            sideDrawer.classList.remove('active')
-        }
-    }
-    firstX = '';
-})
-
-const toggleSubItemActive = (subItem) => {
-    if (subItem !== undefined){
-        subItem.classList.toggle('active');
-    }
-}
-
-// find all titles, add this event listener to them
-let titles = Array.from(document.getElementsByClassName('drawerItemTitle')); 
-let subItems = Array.from(document.getElementsByClassName('subItems'))
-
-titles.forEach(title => {
-    // find the UL that matches this title element (the next sibling element) and add toggle its visitbility with a callback to toggleSubItemActive(the matching UL) 
-    let matchingUL = title.nextElementSibling;
-    console.log(matchingUL);
-    if (matchingUL !== undefined){
-        title.addEventListener('click', () => toggleSubItemActive(matchingUL))
-    }
-})
-
-// on a subitem click, hide the drawer
-subItems.forEach(subItem => {
-    subItem.addEventListener('click', toggleMenuOpen)
-})
+//     if (recentTouches.length > 0){
+//         // console.log(recentTouches)
+//         // console.log(`${event.timeStamp} - ${recentTouches[recentTouches.length-1]}`);
+//         if ((event.timeStamp - recentTouches[recentTouches.length-1]) < 1000){
+//             console.log('double tap')
+//             recentTouches = [];
+//         }
+//     }
+//     recentTouches = [];
+//     recentTouches.push(event.timeStamp); 
+//     console.log(recentTouches);
+//     let touchLocation = event.targetTouches[0]
+//     x1 = touchLocation.clientX;
+//     y1 = touchLocation.clientY;
+//     // debugger
+// })
 
 
-// generate sideDrawer buttons for navigation 
+// document.addEventListener('touchmove', (event)=>{
+//     // console.log(event.touches[0].clientY)
+//     // console.log(event.touches[0].clientX)
+
+//     // toggler.style.left = event.touches[0].pageX + 'px'
+//     // toggler.style.top = event.touches[0].pageY + 'px'
+//     if (event.touches[0].clientX > firstX + 20){
+//         // move drawer out 
+//         sideDrawer.style.left = event.touches[0].clientX;
+//     }
+
+// })
+
+// document.addEventListener('touchend', event => {
+//     let x2 = event.changedTouches[0].clientX; 
+//     let y2 = event.changedTouches[0].clientY; 
+//     let xDiff = Math.abs(x1 - x2); 
+//     let yDiff = Math.abs(y1 - y2); 
+
+//     // if the minimum distance swiped is (whatever the const set at the top of this page says) and the swipe started inside the swipe area (again, see the top of the page for those consts)
+//     // console.log(`${x1} ${x2}`)
+//     // debugger
+//     // if swiping right 
+//     if (x2 - x1 > minimumSwipeDistance && x1 < swipeAreaRight){
+//         if (!sideDrawer.classList.contains('active')){
+//             sideDrawer.classList.add('active')
+//         }
+//     }
+//     // if swiping left 
+//     else if ((x1 - x2 > minimumSwipeDistance) && x1 < drawerWidth && xDiff > yDiff){
+//         // console.log('swiped back')
+//         if (sideDrawer.classList.contains('active')){
+//             sideDrawer.classList.remove('active')
+//         }
+//     }
+//     firstX = '';
+// })
+
+// const toggleSubItemActive = (subItem) => {
+//     if (subItem !== undefined){
+//         subItem.classList.toggle('active');
+//     }
+// }
+
+// // find all titles, add this event listener to them
+// let titles = Array.from(document.getElementsByClassName('drawerItemTitle')); 
+// let subItems = Array.from(document.getElementsByClassName('subItems'))
+
+// titles.forEach(title => {
+//     // find the UL that matches this title element (the next sibling element) and add toggle its visitbility with a callback to toggleSubItemActive(the matching UL) 
+//     let matchingUL = title.nextElementSibling;
+//     console.log(matchingUL);
+//     if (matchingUL !== undefined){
+//         title.addEventListener('click', () => toggleSubItemActive(matchingUL))
+//     }
+// })
+
+// // on a subitem click, hide the drawer
+// subItems.forEach(subItem => {
+//     subItem.addEventListener('click', toggleMenuOpen)
+// })
+
+
+// // generate sideDrawer buttons for navigation 
 const headers = Array.from(document.getElementsByTagName('h2')); 
 
 const generateNavigation = list => {
@@ -135,3 +137,6 @@ const generateNavigation = list => {
     })
 }
 generateNavigation(headers); 
+
+closeMenuButton.addEventListener('click', () => closeSideDrawer()); 
+toggler.addEventListener('click', () => openSideDrawer())
